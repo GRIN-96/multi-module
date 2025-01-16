5e9454206836c6c07b55b66f23aaaa38c2f6184b
GRIN-96 <bnmnil96@gmail.com>
2025-01-16 16:17:54 +0900

    Init: first commit

---
+++ module-common/src/main/java/com/dev/TestBean.java
@@ -0,0 +1,12 @@
+package com.dev;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean {
+
+    public void dependencyTest() {
+        System.out.println("성공적으로 로딩됐습니다.");
+    }
+
+}
